010b9ba91ded9611650b0648973ea63a9d60eb9b
wolwerr <ricardo@dtmm.com.br>
2023-09-28 23:32:07 -0300

    "Conexão com BD e criação de DLL"

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using myfinance_web_dotnet_infra;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MyFinanceDbContext>();
 
 var app = builder.Build();
 
