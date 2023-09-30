1fbf74c86b31b5d42a368e6e15a715553481a5fa
wolwerr <ricardo@dtmm.com.br>
2023-09-30 15:56:29 -0300

    "CRUD completed"

--- 1-) myfinance-web-dotnet/Controllers/PlanoContaController.cs
+++ 1-) myfinance-web-dotnet/Controllers/PlanoContaController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using myfinance_web_dotnet_domain.Entities;
 using myfinance_web_dotnet_service.Interfaces;
 using myfinance_web_netcore.Models;
 
@@ -45,6 +46,59 @@ namespace myfinance_web_netcore.Controllers
             ViewBag.listaPlanoConta = listaPlanoContaModel;
 
             return View();
+        }
+
+        // [HttpGet]
+        // [Route("Cadastrar")]
+        // public IActionResult Cadastrar()
+        // {
+        //     return View();
+        // }   
+
+        [HttpGet]
+        [Route("Cadastrar")]
+        [Route("Cadastrar/{Id}")]
+        public IActionResult Cadastrar(int? Id)
+        {
+            if (Id != null)
+            {
+            var planoConta = _planoContaService.RetornarRegistro((int)Id);
+            var PlanoContaModel = new PlanoContaModel()
+            {
+                Id = planoConta.Id,
+                Descricao = planoConta.Descricao,
+                Tipo = planoConta.Tipo
+            };
+
+            return View(PlanoContaModel);
+            }else{
+                return View();
+            }
+        }   
+        
+        [HttpPost]
+        [Route("Cadastrar")]
+        [Route("Cadastrar/{Id}")]
+        public IActionResult Cadastrar(PlanoContaModel model)
+        {
+            var planoConta = new PlanoConta()
+            {
+                Id = model.Id,
+                Descricao = model.Descricao,
+                Tipo = model.Tipo
+            };
+
+            _planoContaService.Cadastrar(planoConta);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Route("Excluir/{Id}")]
+        public IActionResult Excluir(int? Id)
+        {
+            _planoContaService.Excluir((int)Id);
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
